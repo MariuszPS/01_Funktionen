fda860a860758d099b4023de88c27718292f70e2
Mariusz PS <103026332+MariuszPS@users.noreply.github.com>
2022-05-12 15:09:32 +0200

    Funktionen 01

--- script_01.js
+++ script_01.js
@@ -4,3 +4,32 @@
 
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log ("Grizabella");
+}   
+
+/****** Funktionen 02a ******/
+// 2a. Parametriesierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() {
+    let firstName = "Mariusz";
+    console.log("Hallo " + firstName + "!");
+}
+
+// console.log(firstName);
+
+/****** Funktionen 02b ******/
+// 2b. Parametriesierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2();
+
+function ausgabeNamen2(firstName) {
+    console.log("Hallo " + firstName + "!");
+}
