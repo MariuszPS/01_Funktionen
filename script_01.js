b283b931914d288f70e99c5607a07696b009745b
Mariusz PS <103026332+MariuszPS@users.noreply.github.com>
2022-05-13 14:12:18 +0200

    SRP/Vorbereitung

--- script_01.js
+++ script_01.js
@@ -28,10 +28,39 @@ function ausgabeNamen() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamen2("Bernhard");  // Args ..
-ausgabeNamen2("Mohanned");
-ausgabeNamen2("Bianca");
+// ausgabeNamen2("Bernhard");  // Args ..
+// ausgabeNamen2("Mohanned");
+// ausgabeNamen2("Bianca");
 
 function ausgabeNamen2(firstName) { // Parameter
     console.log("Hallo " + firstName + "!"); 
 }
+
+/******* Funktionen 02c *******/
+// 2c. Mehrere Parameter
+
+// ausgabeNamenParams("Maxine","Mützerich");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+
+function ausgabeNamenParams(firstName,familyName) {
+    console.log("Hallo " + firstName + " " + familyName + "!");
+    }
+
+    /*********Funktionen 03a *********/
+    // 03a. Vorbereitung
+    // Postulat: one funktion = one job (Uncle Bob)
+    // SRP single responsibility principle 
+
+    ausgabeNamenParams("Maxine","Mützerich");
+
+function ausgabeNamenParams(firstName,familyName) {
+    // 1 job: composing
+    let str = "Hallo " + firstName + " " + familyName + "!";
+    // 2 job: output
+        console.log(str);
+  }
+
+
+
+
+  
